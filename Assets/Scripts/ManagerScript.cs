02bb581207af417060adbdddb62f41ed05639228
Ryohei Nanano <matsuhara.9999ryo@gmail.com>
2018-06-06 18:13:20 +0900

    first

---
+++ Assets/Scripts/ManagerScript.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ManagerScript : MonoBehaviour {
+
+	public bool isPlaying;//ゲームプレイ可能かどうかのフラグ
+	[SerializeField] Text infoText;
+	public Text remainText;
+	[SerializeField] Text timeText;
+	[SerializeField] Text clearText;
+
+	[SerializeField] GameObject items;
+
+	public int remain;//残りアイテム数
+
+	float timer;
+
+	// Use this for initialization
+	void Start () {
+		init();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GameManager();
+
+	}
+
+	void GameManager()
+	{
+		if(isPlaying == true)
+		{
+			timer += Time.deltaTime;//タイマー開始
+            timeText.text = "Time : " + timer.ToString("f2");
+
+			if (remain <= 0)//item無くなったら
+            {
+                isPlaying = false;
+				clearText.text = "C L E A R";
+            }
+		}
+		else//isPlaying == false
+		{
+			if (Input.GetKeyDown("space") == true)
+            {
+                isPlaying = true;//spaceが押されたらisPlayingをtrueに
+                Debug.Log("isPlaying == true");
+                infoText.text = "";
+                remainText.text = "Remain : " + remain;
+                items.gameObject.SetActive(true);
+            }
+		}
+	}
+
+    //色々初期化
+	void init()
+	{
+		isPlaying = false;//最初はfalse
+        remainText.text = "";
+        timeText.text = "";
+        clearText.text = "";
+        items.gameObject.SetActive(false);
+
+        remain = items.transform.childCount;//itemの数を変数remainに格納！
+	}
+}
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerScript : MonoBehaviour {
+
+	public Vector2 playerSpeed = new Vector2(0.05f, 0.05f);
+	Rigidbody2D rb2d;
+	[SerializeField] float speed;
+	[SerializeField] float bounciness;
+
+	ManagerScript ms;
+
+	RectTransform rt;
+
+	// Use this for initialization
+	void Start () {
+		rb2d = GetComponent<Rigidbody2D>();//変数rb2dの中にRigidbody2dを格納しておく！
+		rt = GetComponent<RectTransform>();
+
+		ms = GameObject.Find("Manager").GetComponent<ManagerScript>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Move();
+	}
+
+
+    //ReceTransform使って移動させてみる
+	void Move()
+	{
+		if(ms.isPlaying == true)
+		{
+			//x軸方向の入力(左右)を拾う(-1 < x < 1)
+            float x = Input.GetAxisRaw("Horizontal");
+            //Debug.Log("x : " + x);
+
+            rt.localPosition += new Vector3(x * speed * Time.deltaTime, 0, 0);
+		}
+        
+	}
+
+
+	//Playerのバウンドをコントロール
+	private void OnCollisionEnter2D(Collision2D col)
+	{
+        if (col.gameObject.tag == "Ground")
+        {
+            rb2d.velocity = new Vector2(0, bounciness);
+        }
+	}
+
+	private void OnTriggerEnter2D(Collider2D col)
+	{
+		if(col.gameObject.tag == "Item")
+		{
+			Destroy(col.gameObject);
+			ms.remain--;
+			ms.remainText.text = "Remain : " + ms.remain;
+		}
+	}
+}
