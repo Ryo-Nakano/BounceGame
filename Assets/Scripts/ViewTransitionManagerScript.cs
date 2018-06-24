966257fb48dc77f20b3211ccf523b2e218d81c9a
Ryohei Nanano <matsuhara.9999ryo@gmail.com>
2018-06-24 23:27:50 +0900

    ▶︎View2への画面遷移後、右半分だけView1が見える実装完了 ・見えるだけでView1の操作は不可能 ・View2の内容物はまだ

---
+++ Assets/Scripts/ViewTransitionManagerScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ViewTransitionManagerScript : MonoBehaviour {
+
+	[SerializeField] GameObject view2;//View2をUnityからアタッチ
+	Animator animator;//取得したAnimatorを格納しておく為の変数
+
+	[SerializeField] Text changeSkinText;
+	[SerializeField] Text tapToStartText;
+
+	// Use this for initialization
+	void Start () {
+		animator = view2.gameObject.GetComponent<Animator>();//View2についてるAnimatorを取得→変数animatorに格納
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //View2に遷移する為の関数
+	public void GoToView2()
+	{
+		changeSkinText.gameObject.SetActive(false);
+		tapToStartText.gameObject.SetActive(false);
+		animator.SetBool("running", true);
+		Debug.Log("GoToView2");
+	}
+
+    //View1に戻る為の関数
+	public void BackToView1()
+    {
+		animator.SetBool("running", false);
+		changeSkinText.gameObject.SetActive(true);
+        tapToStartText.gameObject.SetActive(true);
+		Debug.Log("BackToView1");
+    }
+}
