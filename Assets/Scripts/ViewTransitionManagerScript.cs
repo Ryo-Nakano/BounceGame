c6095d88da24b1f3c6baa10cff28fe01259c252d
Ryohei Nanano <matsuhara.9999ryo@gmail.com>
2018-06-26 14:59:02 +0900

    ▶︎RankingDataへの画面遷移実装完了 ・Rankingの内容はまだ(後回し)

--- Assets/Scripts/ViewTransitionManagerScript.cs
+++ Assets/Scripts/ViewTransitionManagerScript.cs
@@ -6,8 +6,10 @@ using UnityEngine.UI;
 public class ViewTransitionManagerScript : MonoBehaviour {
 
 	[SerializeField] GameObject view2;//View2をUnityからアタッチ
-	Animator animator;//取得したAnimatorを格納しておく為の変数
-
+	[SerializeField] GameObject view3;//View3をUnityからアタッチ
+	Animator animatorView2;//取得したAnimatorを格納しておく為の変数
+	Animator animatorView3;//取得したAnimatorを格納しておく為の変数
+    
 	[SerializeField] GameObject changeSkinText;
 	[SerializeField] GameObject tapToStartText;
 	[SerializeField] GameObject rankingDataText;
@@ -15,7 +17,8 @@ public class ViewTransitionManagerScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		animator = view2.gameObject.GetComponent<Animator>();//View2についてるAnimatorを取得→変数animatorに格納
+		animatorView2 = view2.gameObject.GetComponent<Animator>();//View2についてるAnimatorを取得→変数animatorに格納
+		animatorView3 = view3.gameObject.GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
@@ -30,18 +33,38 @@ public class ViewTransitionManagerScript : MonoBehaviour {
 		tapToStartText.SetActive(false);
 		rankingDataText.SetActive(false);
 		playerStatus.SetActive(false);
-		animator.SetBool("running", true);
+		animatorView2.SetBool("running", true);
 		Debug.Log("GoToView2");
 	}
 
     //View1に戻る為の関数
 	public void BackToView1()
     {
-		animator.SetBool("running", false);
+		animatorView2.SetBool("running", false);
 		changeSkinText.SetActive(true);
         tapToStartText.SetActive(true);
 		rankingDataText.SetActive(true);
 		playerStatus.SetActive(true);
 		Debug.Log("BackToView1");
+    }
+
+	public void GoToView3()
+    {
+        changeSkinText.SetActive(false);
+        tapToStartText.SetActive(false);
+        rankingDataText.SetActive(false);
+        playerStatus.SetActive(false);
+        animatorView3.SetBool("running", true);
+        Debug.Log("GoToView3");
+    }
+
+	public void BackToView1_2()
+    {
+        animatorView3.SetBool("running", false);
+        changeSkinText.SetActive(true);
+        tapToStartText.SetActive(true);
+        rankingDataText.SetActive(true);
+        playerStatus.SetActive(true);
+        Debug.Log("BackToView1");
     }
 }
